15fede66746c9d464772a68a16a58450aca796df
vunguyen312 <vunguyen312@outlook.com>
2026-08-08 23:26:52 -0400

    Simplified backward to skip weight transposing

---
+++ src/baseNetwork.ts
@@ -0,0 +1,145 @@
+import { Vector, Matrix } from './math';
+
+interface FrozenNeuron {
+    weights: number[];
+    bias: number;
+}
+
+interface FrozenLayer {
+    inputSize: number;
+    outputSize: number;
+    neurons: FrozenNeuron[];
+}
+
+interface FrozenNetwork {
+    learningRate: number;
+    layers: FrozenLayer[]
+}
+
+export abstract class BaseNeuron {
+    protected inputs: number[];
+    protected weights: number[];
+    protected bias: number;
+    protected weightedSum: number;
+
+    constructor(inputSize: number, outputSize: number) {
+        this.inputs = [];
+        this.weights = Vector.randomVector(inputSize, outputSize);
+        this.bias = Math.random();
+        this.weightedSum = 0;
+    }
+
+    protected abstract activation(x: number): number;
+
+    protected abstract activationDerivative(x: number): number;
+
+    public computeDelta(error: number): number {
+        return error * this.activationDerivative(this.weightedSum);
+    }
+
+    public updateParams(learningRate: number, delta: number): void {
+        const updateStep = Vector.scalarMul(learningRate * delta, this.inputs);
+        this.weights = Vector.add(this.weights, updateStep);
+        this.bias += learningRate * delta;
+    }
+
+    public compute(inputs: number[]): number {
+        this.inputs = inputs;
+        this.weightedSum = Vector.dot(inputs, this.weights) + this.bias;
+        const activationValue = this.activation(this.weightedSum);
+        return activationValue;
+    }
+
+    public getWeights(): number[] {
+        return this.weights;
+    }
+
+    public getBias(): number {
+        return this.bias;
+    }
+
+    public getWeightedSum(): number {
+        return this.weightedSum;
+    }
+}
+
+export abstract class BaseLayer<T extends BaseNeuron> {
+    protected inputSize: number;
+    protected outputSize: number;
+    protected neurons: T[];
+    
+    constructor(inputSize: number, outputSize: number) {
+        if (inputSize <= 0) {
+            throw new Error("Layer must have one or more inputs.");
+        }
+
+        if (outputSize <= 0) {
+            throw new Error("Layer must have one or more outputs");
+        }
+
+        this.inputSize = inputSize;
+        this.outputSize = outputSize;
+        this.neurons = this.spawnNeurons();
+    }
+
+    protected abstract spawnNeurons(): T[];
+
+    public forward(inputs: number[]): number[] {
+        return this.neurons.map(neuron => neuron.compute(inputs));
+    }
+
+    public backward(learningRate: number, errors: number[]): number[] {
+        const deltas = this.neurons.map((neuron, i) => 
+            neuron.computeDelta(errors[i])
+        );
+
+        const weights = this.getLayerWeights();
+        const prevErrorsMat = Matrix.mul(weights, [deltas]);
+        const prevErrors = prevErrorsMat[0];
+
+        this.neurons.map((neuron, i) => 
+            neuron.updateParams(learningRate, deltas[i])
+        );
+
+        return prevErrors;
+    }
+
+    private getLayerWeights(): number[][] {
+        return this.neurons.map(neuron => 
+            neuron.getWeights()
+        );
+    }
+}
+
+export abstract class BaseNetwork<T extends BaseNeuron> {
+    protected layers: BaseLayer<T>[];
+    protected learningRate: number;
+
+    constructor(layers: BaseLayer<T>[], learningRate: number) {
+        if (layers.length === 0) {
+            throw new Error("Network must have at least one layer.");
+        }
+
+        this.layers = [...layers];
+        this.learningRate = learningRate;
+    }
+
+    public forwardPass(inputs: number[]): number[] {
+        let valuePassed = [...inputs];
+
+        for (const layer of this.layers) {
+            valuePassed = layer.forward(valuePassed);
+        }
+
+        return valuePassed;
+    }
+
+    public train(inputData: number[], expectedOutput: number[]): void {
+        const output = this.forwardPass(inputData);
+
+        let errors = expectedOutput.map((target, i) => target - output[i]);
+        for (let i = this.layers.length - 1; i >= 0; i--) {
+            errors = this.layers[i].backward(this.learningRate, errors);
+        }
+    }
+}
---
+++ src/math.ts
@@ -0,0 +1,86 @@
+export class Vector {
+    public static randomVector(fanIn: number, fanOut: number): number[] {
+        const a = Math.sqrt(6 / (fanIn + fanOut));
+        const result: number[] = [];
+        for (let i = 0; i < fanIn; i++) {
+            result.push(Math.random() * 2 * a - a);
+        }
+        return result;
+    }
+
+    public static dot(vec1: number[], vec2: number[]): number {
+        if (vec1.length !== vec2.length) {
+            throw new Error('Vectors are not of the same length');
+        }
+
+        let sum = 0;
+        for (let i = 0; i < vec1.length; i++) {
+            sum += vec1[i] * vec2[i];
+        }
+
+        return sum;
+    }
+
+    public static scalarMul(scalar: number, vec: number[]): number[] {
+        const result = [...vec];
+        for (let i = 0; i < vec.length; i++) {
+            result[i] *= scalar;
+        }
+
+        return result;
+    }
+
+    public static add(vec1: number[], vec2: number[]): number[] {
+        if (vec1.length !== vec2.length) {
+            throw new Error('Vectors are not of the same length');
+        }
+
+        const result = [...vec1];
+        for (let i = 0; i < vec1.length; i++) {
+            result[i] += vec2[i];
+        }
+
+        return result;
+    }
+}
+
+export class Matrix {
+    public static zeroMat(rowLength: number, colLength: number): number[][] {
+        const result: number[][] = [];
+        for (let i = 0; i < rowLength; i++) {
+            const col = new Array(colLength).fill(0);
+            result.push(col);
+        }
+
+        return result;
+    }
+
+    public static mul(mat1: number[][], mat2: number[][]): number[][] {
+        const rowLength = mat2.length;
+        const colLength = mat1[0].length;
+        //console.log(mat1, rowLength);
+        //console.log(mat2, colLength);
+        const result = Matrix.zeroMat(rowLength, colLength);
+        for (let i = 0; i < colLength; i++) {
+            const jthRow = Matrix.getRow(mat1, i);
+            for (let j = 0; j < rowLength; j++) {
+                const ithCol = mat2[j];
+                result[j][i] = Vector.dot(jthRow, ithCol);
+            }
+        }
+
+        return result;
+    }
+
+    public static getRow(mat: number[][], j: number): number[] {
+        const result: number[] = [];
+        for (let i = 0; i < mat.length; i++) {
+            result.push(mat[i][j]);
+        }
+
+        return result;
+    }
+}
+
+//console.log(Matrix.mul([[-1, 2], [4, 3]], [[9, 6], [-3, 1]]));
+//throw Error;
