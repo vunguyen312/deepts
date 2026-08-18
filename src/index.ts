f2e2daa02234aa7cfa2ace5780d07ae5713b3ecb
vunguyen312 <vunguyen312@outlook.com>
2026-08-17 20:10:22 -0400

    Add public entry point with named exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+export { NetworkController } from "./core/NetworkController";
+export { Layer, NeuralNetwork } from "./core/neuralNetwork";
+export { SGDOptimizer } from "./core/optimizer";
+export { MNISTParser } from "./utils/MNISTParser";
+
 // Can delete. Just loading in examples
 import "./examples/xor";
 import "./examples/mnist";
