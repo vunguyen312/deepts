5bd7022dbc5985e7331d97769ca043e81bf8b651
vunguyen312 <vunguyen312@outlook.com>
2026-08-22 17:16:32 -0400

    Updated exports and added back examples

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from "./core/networkController";
 export { Layer, NeuralNetwork } from "./core/neuralNetwork";
-export { SGDOptimizer } from "./core/optimizer";
+export { SGD } from "./core/optimizer";
 export { MNISTParser } from "./utils/MNISTParser";
+
+import "./examples/mnist";
+import "./examples/xor";
